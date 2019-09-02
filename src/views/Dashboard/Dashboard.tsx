9524f18f9d70809b5a49fe15b0fef460b4665054
Mariano V <marianov994@gmail.com>
2019-09-01 21:33:51 -0300

    scss and image/svg fixeds

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,6 +1,10 @@
 import React, { Component, Fragment } from 'react';
 import './Dashboard.scss';
 
+import SvgFavorite from './SVGFavorite';
+import SvgSearch from './SVGSearch';
+import PngBannerExample from './banner_one.png';
+
 class Dashboard extends Component {
   render() {
     return (
@@ -19,16 +23,9 @@ class Dashboard extends Component {
           <form className="search__form">
             <input className="search__form__input" type="search" placeholder="Chocolate Cake" />
             <button className="search__form__button" type="submit">
-              <svg className="search__form__button__icon">
-                {/* <use xlink="http://www.w3.org/1999/xlink" href="#icon__search"></use> */}
-              </svg>
+              <SvgSearch className="search__form__button__icon" />
             </button>
           </form>
-          <svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" display="none">
-            <symbol id="icon__search" viewBox="0 0 32 32">
-              <path d="M 19.5 3 C 14.26514 3 10 7.2651394 10 12.5 C 10 14.749977 10.810825 16.807458 12.125 18.4375 L 3.28125 27.28125 L 4.71875 28.71875 L 13.5625 19.875 C 15.192542 21.189175 17.250023 22 19.5 22 C 24.73486 22 29 17.73486 29 12.5 C 29 7.2651394 24.73486 3 19.5 3 z M 19.5 5 C 23.65398 5 27 8.3460198 27 12.5 C 27 16.65398 23.65398 20 19.5 20 C 15.34602 20 12 16.65398 12 12.5 C 12 8.3460198 15.34602 5 19.5 5 z" />
-            </symbol>
-          </svg>
         </section>
         <section id="section_tree">
           <div className="category__header">
@@ -71,6 +68,7 @@ class Dashboard extends Component {
           </div>
           <div className="trending__list">
             <div className="trending__list__article">
+              <SvgFavorite className="trending__list__article__favorite" />
               <img className="trending__list__article__image" src="https://image.flaticon.com/icons/svg/1890/1890012.svg" />
               <div className="trending__list__article__info">
                 <p className="trending__list__article__info__delivery">0.5U$D - 30m</p>
@@ -79,6 +77,7 @@ class Dashboard extends Component {
               </div>
             </div>
             <div className="trending__list__article">
+              <SvgFavorite className="trending__list__article__favorite" />
               <img className="trending__list__article__image" src="https://image.flaticon.com/icons/svg/1365/1365540.svg" />
               <div className="trending__list__article__info">
                 <p className="trending__list__article__info__delivery">0.5U$D - 30m</p>
@@ -87,6 +86,7 @@ class Dashboard extends Component {
               </div>
             </div>
             <div className="trending__list__article">
+              <SvgFavorite className="trending__list__article__favorite" />
               <img className="trending__list__article__image" src="https://image.flaticon.com/icons/svg/135/135646.svg" />
               <div className="trending__list__article__info">
                 <p className="trending__list__article__info__delivery free">FREE - 30m</p>
@@ -105,10 +105,7 @@ class Dashboard extends Component {
           </div>
           <div className="banners__list">
             <article className="banners__list__article">
-              <img src="./../assets/img/banner_one.png" />
-            </article>
-            <article className="banners__list__article">
-              <img src="./../assets/img/banner_two.png" />
+              <img src={PngBannerExample} />
             </article>
           </div>
         </section>
