80499a6ca5f1614606870bbd679e0ed27886d5e2
Mariano Villarreal <marianov994@gmail.com>
2019-11-10 21:52:17 -0300

    dashboard module checked

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,13 +1,18 @@
-import React, { FunctionComponent } from 'react';
-import { BrowserRouter, Route, Switch, BrowserRouterProps } from 'react-router-dom';
+import React, { FunctionComponent } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  BrowserRouterProps
+} from "react-router-dom";
 
-import Search from '../components/views/Search/Search';
-import Article from '../components/views/Article/Article';
-import Checkout from '../components/views/Checkout/Checkout';
-import Dashboard from '../components/views/Dashboard/Dashboard';
-import Category from '../components/views/Category/Category';
+import Search from "../components/views/Search/Search";
+import Article from "../components/views/Article/Article";
+import Checkout from "../components/views/Checkout/Checkout";
+import Dashboard from "../components/views/Dashboard/Dashboard";
+import Category from "../components/views/Category/Category";
 
-import { NoMatch } from '../components/views/404';
+import { NoMatch } from "../components/views/404";
 
 const Router: FunctionComponent<BrowserRouterProps> = () => {
   return (
@@ -21,6 +26,7 @@ const Router: FunctionComponent<BrowserRouterProps> = () => {
         <Route exact path="/category/:categoryName" component={Category} />
 
         {/* <Switch>
+          <Route path="/promo/:id" component={Dashboard} />
           <Route path="/order/new" component={Dashboard} />
           <Route path="/order/info" component={Dashboard} />
           <Route path="/user/:id/profile" render={props => <UserProfile userId={props.match.params.id} />} /> 
